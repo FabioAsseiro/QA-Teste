4a2eb9070a097e95836b87d3f21b002c059df3e3
FabioAsseiro <fabioasseiro@hotmail.com>
2025-07-16 11:35:24 -0300

    feat: new automated test

--- teste-automaticos/cypress1/cypress/e2e/login.cy.js
+++ teste-automaticos/cypress1/cypress/e2e/login.cy.js
@@ -1,14 +1,22 @@
 describe('Login - SauceDemo', () => {
   it('Deve fazer login com usuário válido', () => {
     cy.visit('https://www.saucedemo.com/')
-    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="username"]').type('performance_glitch_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
     cy.url().should('include', '/inventory.html')
     cy.contains('Products').should('be.visible')
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').should('be.visible').click()
+    cy.get('[data-test="shopping-cart-link"]').should('be.visible').click()
+    cy.get('.cart_item').should('have.length', 1)
+    cy.get('[data-test="checkout"]').should('be.visible').click()
+    cy.get('[data-test="firstName"]').type('John')
+    cy.get('[data-test="lastName"]').type('James')
+    cy.get('[data-test="postalCode"]').type('12345')
+    cy.get('[data-test="continue"]').click()
   })
 
-  it('Deve mostrar erro ao tentar login com usuário inválido', () => {
+  it.skip('Deve mostrar erro ao tentar login com usuário inválido', () => {
     cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('standard')
     cy.get('[data-test="password"]').type('secret_sauce')
@@ -16,7 +24,7 @@ describe('Login - SauceDemo', () => {
     cy.get('[data-test="error"]').should('contain', 'Username and password do not match')
   })
 
-  it('Deve mostrar que os campos são obrigatórios', () => {
+  it.skip('Deve mostrar que os campos são obrigatórios', () => {
     cy.visit('https://www.saucedemo.com')
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username is required')
